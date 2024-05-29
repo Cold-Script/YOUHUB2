42a062c3e69ef7e3647ba4a27ff8a9024abf9672
Cold-Script <166982379+Cold-Script@users.noreply.github.com>
2024-05-29 19:41:35 +0700

    Update and rename README.md to Sol's  RNG.lua

---
+++ Sol's  RNG.lua
@@ -0,0 +1,70 @@
+
+-- // Service \\
+local Player2 = game.Players.LocalPlayer.Character
+local Light = game.Lighting
+-- // Execute \\
+if _G.A then
+Light.Brightness = 5
+Light.GlobalShadows = false
+Light.FogEnd = 9e9
+Light.OutdoorAmbient = Color3.new(1,1,1)
+Player2.Humanoid.WalkSpeed = 19
+game.CurrentCamera.FieldOfView = 120
+return
+end
+_G.A = true
+-- // ESP \\
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
+local droppedItemsFolder = game.Workspace:WaitForChild("DroppedItems")
+
+local lines = {} -- Таблица для хранения линий
+
+local function clearLines()
+    for _, line in pairs(lines) do
+        if line then
+            line:Destroy() -- Удаляем старую линию
+        end
+    end
+    lines = {} -- Очищаем таблицу линий
+end
+
+local function drawLineToItem(item)
+    local attachment1
+    if item:IsA("BasePart") then
+        attachment1 = Instance.new("Attachment", item)
+    elseif item:IsA("Model") then
+        local basePart = item:FindFirstChildWhichIsA("BasePart")
+        if basePart then
+            attachment1 = Instance.new("Attachment", basePart)
+        else
+            return -- Если в Model нет BasePart, пропускаем создание линии
+        end
+    else
+        return -- Если объект не BasePart и не Model, пропускаем создание линии
+    end
+
+    local attachment0 = Instance.new("Attachment", humanoidRootPart)
+    local line = Instance.new("Beam")
+    line.Attachment0 = attachment0
+    line.Attachment1 = attachment1
+    line.Color = ColorSequence.new(Color3.fromHSV(tick() % 5/5, 1, 1)) -- Красный цвет линии
+    line.FaceCamera = true
+    line.Parent = humanoidRootPart
+    table.insert(lines, line) -- Добавляем линию в таблицу
+end
+
+local function updateLines()
+    while wait(0.25) do
+        clearLines()
+        for _, item in pairs(droppedItemsFolder:GetDescendants()) do
+            if item:IsA("BasePart") or item:IsA("Model") then
+                drawLineToItem(item)
+            end
+        end
+    end
+end
+
+updateLines()
+
